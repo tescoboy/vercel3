a19de8b5aac401bedc0208a4d6d692e63a2aa847
tescoboy <43451876+tescoboy@users.noreply.github.com>
2025-01-19 01:04:18 +0000

    Add files via upload

---
+++ hello-world-react/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Hello World</h1>
+      <p>Welcome to your React app!</p>
+    </div>
+  );
+}
+
+export default App;
